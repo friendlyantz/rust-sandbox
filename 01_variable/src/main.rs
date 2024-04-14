2859248350debc2bc3d62fd4d25a449134d76863
anton <friendlyantz@pm.me>
2024-04-14 15:52:35 +1000

    Muation, unused vars. Rearrange into functions

--- 01_variable/src/main.rs
+++ 01_variable/src/main.rs
@@ -1,6 +1,17 @@
 // Fix the error below with least amount of modification to the code
 fn main() {
-    let mut x: i32 = 1;
+    mutation();
+    scopes();
+    unused_var();
+}
+
+#[allow(unused_variables)]
+fn unused_var() {
+    let x = 1;
+}
+
+fn scopes() {
+    let x = 1;
     {
         let y: i32 = 5;
         println!("Inner scope value of x is {} and value of y is {}", x, y);
@@ -8,8 +19,12 @@ fn main() {
     let y: i32 = 6;
 
     println!("Inner scope value of x is {} and value of y is {}", x, y);
+}
+
+fn mutation() {
+    let mut x: i32 = 1;
     x += 4;
 
     assert_eq!(x, 5);
-    println!("Success!");
+    println!("Mutation Success!");
 }
