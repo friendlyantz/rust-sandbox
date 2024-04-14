e0f17f43dd8b61f4088cf04f570319bf093e512e
anton <friendlyantz@pm.me>
2024-04-14 15:01:10 +1000

    Learn mutables

--- 01_variable/src/main.rs
+++ 01_variable/src/main.rs
@@ -1,7 +1,14 @@
 // Fix the error below with least amount of modification to the code
 fn main() {
-    let x: i32; // Uninitialized but used, ERROR !
-    let y: i32; // Uninitialized but also unused, only a Warning !
+    let mut x: i32 = 1;
+    {
+        let y: i32 = 5;
+        println!("Inner scope value of x is {} and value of y is {}", x, y);
+    }
+    let y: i32 = 6;
+
+    println!("Inner scope value of x is {} and value of y is {}", x, y);
+    x += 4;
 
     assert_eq!(x, 5);
     println!("Success!");
